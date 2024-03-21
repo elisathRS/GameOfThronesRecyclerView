e76513707981214bb273b6ca964d2775017307a3
Mary E Ramirez <mary0605@gmail.com>
2024-03-20 21:17:26 -0400

    Second Commit

---
+++ app/src/main/java/com/example/gameofthronesviewer/CharacterAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.gameofthronesviewer
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class CharacterAdapter(private val context: Context, private val characterList: List<Pair<String, Triple<String, String, String>>>) : RecyclerView.Adapter<CharacterAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.character_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val (imageUrl, characterInfo) = characterList[position]
+        holder.bind(imageUrl, characterInfo)
+
+        holder.imageView.setOnClickListener {
+            Toast.makeText(holder.itemView.context, "Character at position $position clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return characterList.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imageViewCharacter)
+        private val tvFullName: TextView = itemView.findViewById(R.id.tvFullNameCharacter)
+        private val tvTitle: TextView = itemView.findViewById(R.id.tvTitleCharacter)
+        private val tvFamily: TextView = itemView.findViewById(R.id.tvFamilyCharacter)
+
+        fun bind(imageUrl: String, characterInfo: Triple<String, String, String>) {
+            Glide.with(context)
+                .load(imageUrl)
+                .centerCrop()
+                .into(imageView)
+            tvFullName.text = characterInfo.first
+            tvTitle.text = characterInfo.second
+            tvFamily.text = characterInfo.third
+        }
+    }
+}
